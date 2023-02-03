52a561140ffdd8b58108a3ce5b83803e2a7b41a4
Rafael Victor <rafaelv.boscato@hotmail.com>
2023-02-03 09:20:49 -0300

    add component TopBar

---
+++ src/components/PageInitial/TopBar/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {
+    View,
+    Image
+} from 'react-native'
+import styles from './style';
+
+export default function TopBar() {
+    return (
+        <View style={styles.BarraSuperior}>
+            <Image source={require('../../../image/logoInterna.png')} style={styles.image} />
+        </View>
+    );
+}
---
+++ src/components/PageInitial/TopBar/style.js
@@ -0,0 +1,20 @@
+import { StyleSheet } from "react-native";
+const styles = StyleSheet.create({
+    BarraSuperior: {
+        backgroundColor: "black",
+        width: "100%",
+        height: 90,
+        alignItems:"center",
+        position:"relative",
+    },
+    image: {
+        alignItems: "center",
+        justifyContent: "center",
+        padding: 10,
+        width: "70%",
+        height: 90,
+        backgroundColor:"black",
+    }
+});
+
+export default styles
