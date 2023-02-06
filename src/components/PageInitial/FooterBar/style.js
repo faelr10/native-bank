2e79c1f15f6179fffabfb07bfdad082913e9df94
Rafael Victor <rafaelv.boscato@hotmail.com>
2023-02-06 08:58:35 -0300

    add component Footer

--- src/components/PageInitial/FooterBar/style.js
+++ src/components/PageInitial/FooterBar/style.js
@@ -2,27 +2,26 @@ import { StyleSheet } from "react-native";
 const styles = StyleSheet.create({
     boxFooterBar: {
         width: "100%",
-        height: "12%",
-        borderWidth: 1,
+        height: "10%",
         position: 'absolute',
         left: 0,
         right: 0,
-        bottom: 0,
-        backgroundColor: "black",
+        bottom: -1,
+        backgroundColor: "#1b1e23",
         justifyContent:"space-around",
         flexDirection:"row",
         alignItems:"center"
     },
     fieldIconFooter:{
-        width:"17%",
+        width:"18%",
         backgroundColor:"#d3d3d3",
-        height:"75%",
+        height:"80%",
         borderRadius:50,
         alignItems:"center",
         justifyContent:"center"
     },
     iconFooter: {
-        fontSize:30,
+        fontSize:20,
         color: "#000",
     }
 });
